84bd548ed146f5155c375697a09d2dc1fd4cf78f
Andrearttr <148363532+Andrearttr@users.noreply.github.com>
2025-05-19 15:31:46 +0200

    Aggiunta lezione 2025-05-19 con directory corrette

---
+++ 2025-05-19/ClassiEOggetti/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Persona
+{
+    public string? Nome;
+    public int Eta;
+
+    public Persona(string nome, int eta)
+    {
+        Nome = nome;
+        Eta = eta;
+    }
+
+    public void Presentati()
+    {
+        Console.WriteLine($"Ciao, sono {Nome} e ho {Eta} anni.");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Persona p = new Persona("Andrea", 21);
+        p.Presentati();
+    }
+}
