5acab860a25d8d51f485bbbc151757c5723b0dd1
Andrearttr <andrearottura03@gmail.com>
2025-05-28 15:05:48 +0200

    Aggiunto esercizio 2 factory

---
+++ 2025-05-28/Esercizio2Factory/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+public interface IShape
+{
+    void Draw();
+}
+
+public class Circle : IShape
+{
+    public void Draw()
+    {
+        Console.WriteLine("\n  * *  ");
+        Console.WriteLine("*     *");
+        Console.WriteLine("*     *");
+        Console.WriteLine("  * *  ");
+    }
+}
+
+public class Square : IShape
+{
+    public void Draw()
+    {
+        Console.WriteLine("\n* * * *");
+        Console.WriteLine("*     *");
+        Console.WriteLine("*     *");
+        Console.WriteLine("* * * *");
+    }
+}
+
+public abstract class ShapeCreator
+{
+    public abstract IShape CreateShape();
+}
+
+public class ConcreteCircleCreator : ShapeCreator
+{
+    public override IShape CreateShape()
+    {
+        return new Circle();
+    }
+}
+
+public class ConcreteSquareCreator : ShapeCreator
+{
+    public override IShape CreateShape()
+    {
+        return new Square();
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("\nQuale forma vuoi costruire (circle, square): ");
+        string type = Input.String();
+
+        ShapeCreator shapeCreator = null;
+
+        switch (type.ToLower())
+        {
+            case "circle":
+                shapeCreator = new ConcreteCircleCreator();
+                break;
+
+            case "square":
+                shapeCreator = new ConcreteSquareCreator();
+                break;
+
+            default:
+                Console.WriteLine("Forma non riconosciuta");
+                break;
+        }
+
+        if (shapeCreator != null)
+        {
+            IShape shape = shapeCreator.CreateShape();
+            shape.Draw();
+        }
+    }
+}
