82d055cf408a834aed64f08dbba9c9691b3665ce
Andrearttr <andrearottura03@gmail.com>
2025-06-03 10:46:02 +0200

    Aggiunti Esercizi 3 e 4 Dependency Injection

---
+++ 2025-06-03/Esercizio3DInjection/Program.cs
@@ -0,0 +1,44 @@
+﻿public interface ILogger
+{
+    void Log(string message);
+}
+
+public class Logger : ILogger
+{
+    public void Log(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
+
+public class Printer
+{
+    public ILogger Logger { get; set; }
+
+    public void Print(string message)
+    {
+        if (Logger == null)
+        {
+            Console.WriteLine("Logger non impostato");
+        }
+        else
+        {
+            Logger.Log(message);
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var logger = new Logger();
+        var printer = new Printer();
+
+        printer.Print("Prova senza logger");
+
+        printer.Logger = logger;
+
+        printer.Print("prova con logger");
+    }
+}
---
+++ 2025-06-03/Esercizio4DInjection/Program.cs
@@ -0,0 +1,80 @@
+﻿public interface IStorageService
+{
+    void Upload(string file);
+}
+
+public class DiskStorage : IStorageService
+{
+    public void Upload(string file)
+    {
+        Console.WriteLine($"il file {file} è stato caricato sul disco");
+    }
+}
+
+public class MemoryStorage : IStorageService
+{
+    public void Upload(string file)
+    {
+        Console.WriteLine($"il file {file} è stato caricato sulla memoria");
+    }
+}
+
+public class FileUploader
+{
+    public IStorageService storageService { get; set; }
+
+    public void Upload(string file)
+    {
+        if (storageService == null)
+        {
+            Console.WriteLine("Nessun servizio selezionato");
+        }
+        else
+        {
+            storageService.Upload(file);
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var diskStorage = new DiskStorage();
+        var memoryStorage = new MemoryStorage();
+        var fileUploader = new FileUploader();
+
+        bool exit = false;
+        
+        while (!exit)
+        {
+            Console.WriteLine("\nMenù");
+            Console.WriteLine("[1] Carica su disco");
+            Console.WriteLine("[2] Carica su memoria");
+            Console.WriteLine("[0] Esci");
+            Console.Write("Scelta: ");
+            int menuAction = Input.Int(0, 2);
+        
+            switch (menuAction)
+            {
+                case 1:
+                    Console.Write("inserisci file: ");
+                    string file = Input.String();
+                    fileUploader.storageService = diskStorage;
+                    fileUploader.Upload(file);
+                    break;
+        
+                case 2:
+                    Console.Write("inserisci file: ");
+                    file = Input.String();
+                    fileUploader.storageService = memoryStorage;
+                    fileUploader.Upload(file);
+                    break;
+
+                case 0:
+                    exit = true;
+                    break;
+            }
+        }
+    }
+}
