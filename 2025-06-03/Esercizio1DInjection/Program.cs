45727b8d06e6387d15492bd2fa7462ab26b50bf7
Andrearttr <andrearottura03@gmail.com>
2025-06-03 09:55:28 +0200

    Aggiunto Esercizio 1 e 2 Dependency Injection

---
+++ 2025-06-03/Esercizio1DInjection/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public interface IGreeter
+{
+    void Greet(string nome);
+}
+
+public class ConsoleGreeter : IGreeter
+{
+    public void Greet(string nome)
+    {
+        Console.WriteLine($"ciao {nome}!");
+    }
+}
+
+public class GreetingService
+{
+    private readonly IGreeter _greeter;
+
+    public GreetingService(IGreeter greeter)
+    {
+        _greeter = greeter;
+    }
+
+    public void GreetUser(string nome)
+    {
+        _greeter.Greet(nome);
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        IGreeter greeter = new ConsoleGreeter();
+        GreetingService greetingService = new GreetingService(greeter);
+        Console.Write("Inserisci nome: ");
+        string nome = Input.String();
+        greetingService.GreetUser(nome);
+    }
+}
---
+++ 2025-06-03/Esercizio2DInjection/Program.cs
@@ -0,0 +1,77 @@
+﻿public interface IPaymentGateway
+{
+    void Pay(double amount);
+}
+
+public class PaypalGateway : IPaymentGateway
+{
+    public void Pay(double amount)
+    {
+        Console.WriteLine($"pagamento di {amount} euto tramite paypal.");
+    }
+}
+
+public class StripeGateway : IPaymentGateway
+{
+    public void Pay(double amount)
+    {
+        Console.WriteLine($"pagamento di {amount} euto tramite stripe.");
+    }
+}
+
+public class PaymentProcessor
+{
+    private readonly IPaymentGateway _paymentGateway;
+
+    public PaymentProcessor(IPaymentGateway paymentGateway)
+    {
+        _paymentGateway = paymentGateway;
+    }
+
+    public void PayWithGateway(double amount)
+    {
+        _paymentGateway.Pay(amount);
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var paypalGateway = new PaypalGateway();
+        var stripeGateway = new StripeGateway();
+        PaymentProcessor paymentProcessor;
+        bool exit = false;
+        
+        while (!exit)
+        {
+            Console.WriteLine("\nMenù");
+            Console.WriteLine("[1] Paga con paypal");
+            Console.WriteLine("[2] Paga con stripe");
+            Console.WriteLine("[0] Esci");
+            Console.Write("Scelta: ");
+            int menuAction = Input.Int(0, 2);
+        
+            switch (menuAction)
+            {
+                case 1:
+                    Console.Write("Inserisci somma: ");
+                    double amount = Input.Double();
+                    paymentProcessor = new PaymentProcessor(paypalGateway);
+                    paymentProcessor.PayWithGateway(amount);
+                    break;
+        
+                case 2:
+                    Console.Write("Inserisci somma: ");
+                    amount = Input.Double();
+                    paymentProcessor = new PaymentProcessor(stripeGateway);
+                    paymentProcessor.PayWithGateway(amount);
+                    break;
+
+                case 0:
+                    exit = true;
+                    break;
+            }
+        }
+    }
+}
