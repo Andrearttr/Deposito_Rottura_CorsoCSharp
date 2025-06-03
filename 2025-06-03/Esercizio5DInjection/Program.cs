b519e683c2c817059a10ff5a46da640554f0ecef
Andrearttr <andrearottura03@gmail.com>
2025-06-03 11:22:15 +0200

    Aggiunti Esercizi 5 e 6 Dependency Injection

---
+++ 2025-06-03/Esercizio5DInjection/Program.cs
@@ -0,0 +1,31 @@
+﻿public interface INotifier
+{
+    void Notify(string message);
+}
+
+public class SmsNotifier : INotifier
+{
+    public void Notify(string message)
+    {
+        Console.WriteLine($"Notifica sms: {message}");
+    }
+}
+
+public class AlertService
+{
+    public void SendAlert(string message, INotifier notifier)
+    {
+        notifier.Notify(message);
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var smsNotifier = new SmsNotifier();
+        var alertService = new AlertService();
+
+        alertService.SendAlert("allerta pioggia", smsNotifier);
+    }
+}
---
+++ 2025-06-03/Esercizio6DInjection/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+public interface IExportFormatter
+{
+    void Export(string file);
+}
+
+public class JsonExporter : IExportFormatter
+{
+    public void Export(string file)
+    {
+        Console.WriteLine($"{file} esportato come .Json");
+    }
+}
+
+public class XmlExporter : IExportFormatter
+{
+    public void Export(string file)
+    {
+        Console.WriteLine($"{file} esportato come .xml");
+    }
+}
+
+public class DataExporter
+{
+    public void Export(string file, IExportFormatter exportFormatter)
+    {
+        exportFormatter.Export(file);
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var jsonExporter = new JsonExporter();
+        var xmlExporter = new XmlExporter();
+        var dataExporter = new DataExporter();
+
+        bool exit = false;
+        
+        while (!exit)
+        {
+            Console.WriteLine("\nMenù");
+            Console.WriteLine("[1] Esporta in .json");
+            Console.WriteLine("[2] Esporta in .xml");
+            Console.WriteLine("[0] Esci");
+            Console.Write("Scelta: ");
+            int menuAction = Input.Int();
+        
+            switch (menuAction)
+            {
+                case 1:
+                    Console.Write("Inserisci file: ");
+                    string file = Input.String();
+                    dataExporter.Export(file, jsonExporter);
+                    break;
+        
+                case 2:
+                    Console.Write("Inserisci file: ");
+                    file = Input.String();
+                    dataExporter.Export(file, xmlExporter);
+                    break;
+        
+                case 0:
+                    exit = true;
+                    break;
+            }
+        }
+    }
+}
